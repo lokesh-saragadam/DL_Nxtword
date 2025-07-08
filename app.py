e559382dc3cd30b45859ea2735c225f10b9a5840
lokesh-saragadam <lokeshs2k6@gmail.com>
2025-07-08 14:49:31 +0530

    model changed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 #Load the LSTM Model
-model=load_model('next_word_lst.keras')
+model=load_model('next_word_lstm.h5')
 
 #Load the tokenizer
 with open('tokenizer.pickle','rb') as handle:
